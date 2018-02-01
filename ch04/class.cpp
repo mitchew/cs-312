4a27a5581c712831d52782119aec98f213df30ec
Mitchell Wilson <mewilson@lcmail.lcsc.edu>
2018-01-31 19:13:45 -0800

    class.exe takes a number grade returns letter

--- ch04/class.cpp
+++ ch04/class.cpp
@@ -3,6 +3,28 @@
 using namespace std;
 int main()
 {
-    double num4 = 5152.1613;
-    cout << setw(1) << num4 << "    ";
+    int grade;
+    cout << "Enter grade: ";
+    cin >> grade;
+    cin.ignore();
+    if (grade >= 90 && grade <= 100)
+    {
+        cout << "Grade is A";
+    }
+    else if (grade >= 80 && grade < 90)
+    {
+        cout << "Grade is B";
+    }
+    else if (grade >= 70 && grade < 80)
+    {
+        cout << "Grade is C";
+    }
+    else if (grade >= 60 && grade < 70)
+    {
+        cout << "Grade is D";
+    }
+    else
+    {
+        cout << "Grade is F";
+    }
 }
