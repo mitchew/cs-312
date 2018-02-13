17b7564dab861c25caf1d84591e74a36f467ca89
Mitchell Wilson <mitchw@crcasino.com>
2018-02-12 20:06:53 -0800

    5.21.cpp not yet complete

--- ch05/5.21.cpp
+++ ch05/5.21.cpp
@@ -5,21 +5,48 @@ using namespace std;
 int main()
 {
     int max_width;
-    int current_width;
-    int past_mid_point = 0;
+    int breakpoint = false;
 
     cin >> max_width;
 
     cout << "Diamond Width: " << max_width << endl;
-
-    for (int i = 1; i <= max_width; i++)
+    
+    for (int i = 1; i <= max_width; i += 2)
     {
-        int white_space = (max_width - 1) / 2;
-        for (int j = 0; j < white_space; j++)
+        int whitespace;
+
+        if (i != max_width || i == max_width)
         {
-            cout << " ";
+            whitespace = (max_width - i) /2;
+            for (int k = 1; k <= whitespace; k++)
+            {
+                cout << " ";
+            }
+            for (int j = 1; j <= i; j++)
+            {
+                cout << "+";
+            }
         }
 
+        cout << endl;
+    }
+
+    for (int i = (max_width / 2); i >= 0; i -= 2)
+    {
+        int whitespace;
+
+        if (i != max_width)
+        {
+            whitespace = (max_width - i) /2;
+            for (int k = 1; k <= whitespace; k++)
+            {
+                cout << " ";
+            }
+            for (int j = 1; j <= i; j++)
+            {
+                cout << "+";
+            }
+        }
 
         cout << endl;
     }
