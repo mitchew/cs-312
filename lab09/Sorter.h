ffe82a18c4de9d9de62897334daa6f9e62a55772
Mitchell Wilson <mewilson@lcmail.lcsc.edu>
2018-03-21 23:35:41 -0700

    lab09 completed

---
+++ lab09/Sorter.h
@@ -0,0 +1,12 @@
+#ifndef SORTER_H
+#define SORTER_H
+class Sorter
+{
+    private:
+
+    public:
+    int sortNums(int [], int);
+    int searchNums(int [], int , int, int);
+    void displayArray(int [], int);
+};
+#endif
--- lab09/main.cpp
+++ lab09/main.cpp
@@ -4,17 +4,16 @@
 
 #include <iostream>
 #include <time.h>
+#include "Sorter.h"
 
 using namespace std;
 
 const int SIZE = 20;
 int nums[SIZE];
-void displayArray(int [], int);
-int searchNums(int[], int, int, int);
-int sortNums(int [], int);
 
 int main()
 {
+    Sorter sorter;
     int findMe = 0;
 
     srand(time(NULL));
@@ -25,65 +24,37 @@ int main()
         nums[i] = randNum;
     }
 
-    int swaps = sortNums(nums, SIZE);
+    sorter.displayArray(nums, SIZE);
 
-    cout << "Number of swaps: " << swaps << endl;
+    int swaps = sorter.sortNums(nums, SIZE);
 
-    cout << "Enter a number to find: ";
-    cin >> findMe;
+    bool done = false;
+    bool found = false;
 
-    displayArray(nums, SIZE);
+    cout << "Number of swaps to sort: " << swaps << endl << endl;
+
+    while (!done)
+    {
+        sorter.displayArray(nums, SIZE);
+        
+        cout << "Enter a number to find (0-99) -1 to exit: ";
+        cin >> findMe;
+
+        if (findMe == -1)
+        {
+            done = true;
+            continue;
+        }
+
+        cout << endl;
+        // our array is size 20 so our indexes start at 0 and end at 19
+        int searching = sorter.searchNums(nums, 0, 19, findMe);
+
+        if (searching == -1)
+        {
+            cout << findMe << " is not found" << endl;
+        }
+    }
 
     return 0;
 }
-
-void displayArray(int arr[], int size)
-{
-    for (int i = 0; i < size; i++)
-    {
-        cout << nums[i] << endl;
-    }
-}
-
-int searchNums(int arr[], int right, int left, int find)
-{
-    if (right >= left)
-    {
-        int mid = (right + left) / 2;
-
-        if (arr[mid] == find)
-        {
-            return mid;
-        }
-        if (arr[mid] > find)
-        {
-            return searchNums(arr, right - 1, left, find);
-        }
-
-        return searchNums(arr, right, left + 1, find);
-    }
-
-    return -1;
-}
-
-int sortNums(int arr[], int size)
-{
-    int count;
-
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < size; j++)
-        {
-            if (arr[j] > arr[j+1])
-            {
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-                count++;
-            }
-        }
-    }
-
-    return count;
-}
-
